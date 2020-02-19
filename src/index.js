3fd41ee98a42c61d54a16fc52d224a2e4e6f97b3
Jeremy <Jrmyrosenberg@gmail.com>
2020-02-19 15:37:06 -0800

    working collisions and motion

--- src/index.js
+++ src/index.js
@@ -2,31 +2,42 @@ const MovingObject = require("./movingObject.js");
 
 const objectArray = []
 
-objectArray.push(new MovingObject({
-  pos: [30, 30],
-  vel: [10, 10],
-  radius: 20,
-  color: "#00FF00"
-}));
+colorHash = {
+    '0': 'red',
+    '1': 'green',
+    '2': 'blue',
+    '3': 'orange'
+}
 
+colorCalculator = function(){
+    let num = (Math.floor(Math.random()*4));
+    color = colorHash[num.toString()];
+    return color;
+}
+
+for(let i=0; i<10; i++){
 objectArray.push(
   new MovingObject({
-    pos: [30, 70],
-    vel: [20, 20],
-    radius: 40,
-    color: "#00FF00"
+    pos: [Math.random() * 400, Math.random() * 400],
+    vel: [Math.random() * 10, Math.random() * 10],
+    radius: 20,
+    color: colorCalculator(),
+    game: 'THIS IS A PLACEHOLDER'
   })
 );
+}
+
 
 
 document.addEventListener("DOMContentLoaded", function() {
 
   const canvasEl = document.getElementById("game-canvas");
-  canvasEl.width = 1000;
-  canvasEl.height = 1000;
-
+  canvasEl.width = 500;
+  canvasEl.height = 500;
   const ctx = canvasEl.getContext("2d");
-  objectArray.forEach((el) => {
+  
+  
+  const draw = function(){objectArray.forEach((el) => {
       ctx.beginPath();
       ctx.arc(el.pos[0], el.pos[1], el.radius, 0, 2 * Math.PI, true);
       ctx.strokeStyle = el.color;
@@ -34,8 +45,29 @@ document.addEventListener("DOMContentLoaded", function() {
       ctx.stroke();
       ctx.fillStyle = el.color;
       ctx.fill();
-                              })
+    })
+}
+const asteroidMove = function() {objectArray.forEach((el) => {
+    el.move();
+    // setInterval(() => console.log('hello'), 3000);
+})
+}
 
+const checkAllCollision = function() {
+    for(let i=0; i<objectArray.length; i++){
+        for(let j=i+1; j<objectArray.length; j++){
+            objectArray[i].checkForCollision(objectArray[j])
+        }
+    }
+}
+setInterval(() => draw(ctx.clearRect(0, 0, canvasEl.width, canvasEl.height)), 100);
+setInterval(() => draw(), 100);
+setInterval(() => asteroidMove(), 100);
+setInterval(() => checkAllCollision(), 100);
+
+// setInterval(() => ctx.clearRect(0, 0, 1000, 1000), 1000);
+
+    // debugger;
 
 //   ctx.beginPath();
 //   ctx.arc(100, 100, 20, 0, 2 * Math.PI, true);
--- src/movingObject.js
+++ src/movingObject.js
@@ -6,6 +6,30 @@ function MovingObject(options) {
   this.game = options.game;
 }
 
+MovingObject.prototype.move = function move(){
+   this.pos = [this.pos[0] + this.vel[0], this.pos[1] + this.vel[1]]
+
+   if(this.pos[0] > 500){
+       this.pos = [this.pos[0] = 0, this.pos[1]]
+   } else if (this.pos[1] > 500) {
+    this.pos= [this.pos[0], this.pos[1]=0]
+   }
+};
+
+MovingObject.prototype.checkForCollision = function checkForCollision(object){
+    const circle1 = { radius: this.radius, x: this.pos[0], y: this.pos[1] };
+    const circle2 = { radius: object.radius, x: object.pos[0], y: object.pos[1] };
+
+    const dx = circle1.x - circle2.x;
+    const dy = circle1.y - circle2.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+
+    if (distance < circle1.radius + circle2.radius) {
+      console.log('collision!!!!!!!!!!')
+    }
+}
+
+
 // MovingObject.prototype.draw = function draw() {
 
 //     const ctx = canvasEl.getContext("2d");
